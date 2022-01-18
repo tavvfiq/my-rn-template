72949090f3fb187ec2b603a7d51ccbdbe950723b
tavvfiq <taufiq.w@mail.ugm.ac.id>
2022-01-18 22:29:02 +0700

    feat: add global modal component

---
+++ src/core/ui/modal/Modal.ts
@@ -0,0 +1,98 @@
+import {
+  Layout,
+  Navigation,
+  OptionsModalPresentationStyle,
+} from 'react-native-navigation';
+import { dispatch, listen } from '~core/utils/event';
+import { CLOSE_MODAL_EVENT, DISMISS_MODAL_EVENT } from './constant';
+import ModalComponent, { ModalComponentProps } from './ModalComponent';
+
+const notInitialized = 'You have not initialized RNNBottomSheet component.';
+const openedInstance =
+  'You already have running instance of the component. Aborting...';
+
+export default class Modal {
+  private static _modalOpened = false;
+  private static _registered = false;
+  private static _modalName = '__globalModal__';
+
+  static getComponentName() {
+    return this._modalName;
+  }
+
+  static isOpened() {
+    return this._modalOpened;
+  }
+
+  static init() {
+    if (!this._registered) {
+      // registering modal
+      Navigation.registerComponent(this._modalName, () => ModalComponent);
+      this._registered = true;
+      listen(CLOSE_MODAL_EVENT, () => {
+        this._modalOpened = false;
+      });
+    }
+  }
+
+  /**
+   * open the modal
+   * @param props props to be passed to ModalComponent
+   * @returns void
+   */
+  static open(props: ModalComponentProps) {
+    if (!this._registered) {
+      console.error(notInitialized);
+      return;
+    }
+    if (this._modalOpened) {
+      console.error(openedInstance);
+      return;
+    }
+    this._modalOpened = true;
+    const layout: Layout<ModalComponentProps> = {
+      component: {
+        id: this._modalName,
+        passProps: props,
+        name: this._modalName,
+        options: {
+          animations: {
+            showModal: {
+              enabled: true,
+            },
+            dismissModal: {
+              enabled: true,
+            },
+          },
+          layout: {
+            backgroundColor: 'transparent',
+            componentBackgroundColor: 'transparent',
+          },
+          hardwareBackButton: {
+            dismissModalOnPress: false,
+          },
+          modal: {
+            swipeToDismiss: false,
+          },
+          popGesture: false,
+          modalPresentationStyle:
+            'overCurrentContext' as OptionsModalPresentationStyle,
+        },
+      },
+    };
+    Navigation.showModal(layout);
+  }
+
+  /**
+   * close the modal
+   * @returns void
+   */
+  static close() {
+    if (!this._registered) {
+      console.error(notInitialized);
+      return;
+    }
+
+    dispatch(DISMISS_MODAL_EVENT);
+  }
+}
---
+++ src/core/ui/modal/ModalComponent.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react';
+import { Pressable, StyleSheet } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { dispatch } from '~core/utils/event';
+import { CLOSE_MODAL_EVENT, MODAL_SHOW_ANIMATION_DURATION } from './constant';
+
+/**
+ * Modal Component Props
+ */
+export interface ModalComponentProps {
+  /**
+   * content to be displayed in modal
+   * @type {Function}
+   * @return {React.ReactNode}
+   */
+  renderContent?: () => React.ReactNode;
+  /**
+   * opacity of the backdrop
+   */
+  backdropOpacity?: number;
+  /**
+   * is modal can be dismissed when touching backdrop?
+   */
+  dimissable?: boolean;
+}
+
+type Props = ModalComponentProps & {
+  componentId: any;
+};
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+const ModalComponent = ({
+  renderContent,
+  backdropOpacity = 0.4,
+  dimissable,
+  componentId,
+}: Props) => {
+  const show = useSharedValue(false);
+
+  const style = useAnimatedStyle(() => {
+    const backgroundColor = `rgba(0,0,0,${backdropOpacity})`;
+    return {
+      backgroundColor,
+      opacity: show.value
+        ? withTiming(1, { duration: MODAL_SHOW_ANIMATION_DURATION })
+        : withTiming(0, { duration: MODAL_SHOW_ANIMATION_DURATION }),
+    };
+  });
+
+  useEffect(() => {
+    show.value = true;
+  }, [show]);
+
+  const dismissModal = function () {
+    dispatch(CLOSE_MODAL_EVENT);
+    show.value = false;
+    setTimeout(() => {
+      Navigation.dismissModal(componentId);
+    }, MODAL_SHOW_ANIMATION_DURATION);
+  };
+
+  const backdropOnPress = function () {
+    if (dimissable && show.value) {
+      dismissModal();
+    }
+  };
+
+  return (
+    <AnimatedPressable
+      onPress={backdropOnPress}
+      style={[styles.container, style]}>
+      {renderContent && renderContent()}
+    </AnimatedPressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default ModalComponent;
